0f1276eef7b9a78d81afb289f0425e672d45f155
YHR <h_yacine@live.fr>
2022-07-03 19:51:49 +0200

    Integrate `ProductList`

---
+++ src/components/Products/Product.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react"
+import styles from "./Product.module.scss"
+import Image from "next/image"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMinusSquare, faPlusSquare } from "@fortawesome/free-solid-svg-icons"
+import { useIsOnCompact } from "@/hooks/useIsOnCompact"
+
+interface PropTypes {
+  name: string
+  price: number
+  quantity: number
+  quantityUnit: string
+}
+
+const Product = ({ name, price, quantity, quantityUnit }: PropTypes) => {
+  const [amount, setAmount] = useState(0)
+
+  const isCompact = useIsOnCompact()
+
+  const handleIncrement = () => setAmount(prev => prev + 1)
+  const handleDecrement = () => {
+    if (amount === 0) return
+    setAmount(prev => prev - 1)
+  }
+
+  return (
+    <div className={styles.productCard}>
+      <div className={styles.product}>
+        <div className={styles.image}>
+          {
+            // Show 9+ if more than 9 units of the product were ordered
+            !!amount && (
+              <span className={styles.cartQuantity}>
+                {amount > 9 ? "9+" : amount}
+              </span>
+            )
+          }
+          <Image
+            src="/product.png"
+            alt="Image d'un produit alimentaire"
+            title="Image d'un produit alimentaire"
+            objectFit="cover"
+            width={180}
+            height={240}
+            quality={60}
+          />
+        </div>
+        <div className={styles.info}>
+          <h3 className={styles.name}>{name}</h3>
+          <div className={styles.subInfo}>
+            <p className={styles.price}>{price}€</p>
+            <p className={styles.quantity}>
+              ({quantity} {quantityUnit})
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.cta}>
+        {
+          // Don't show the - button if product hasn't been ordered
+          !!amount && (
+            <FontAwesomeIcon
+              className={styles.minusIcon}
+              icon={faMinusSquare}
+              title="Soustraire du panier"
+              onClick={handleDecrement}
+            />
+          )
+        }
+        <FontAwesomeIcon
+          className={styles.plusIcon}
+          icon={faPlusSquare}
+          title="Ajouter au panier"
+          onClick={handleIncrement}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Product
---
+++ src/components/Products/ProductList.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react"
+import Product from "./Product"
+import data from "@/data/data.json"
+import styles from "./ProductList.module.scss"
+import Searchbar from "../Searchbar/Searchbar"
+import { normaliseString } from "@/utilities/string"
+import { ProductType } from "@/types/Product"
+import { sortByProperty } from "@/utilities/array"
+import NotFoundImage from "@/svg/NotFoundImage"
+
+const ProductList = () => {
+  const { allProducts } = data
+
+  const [filteredProducts, setFilteredProducts] = useState(allProducts)
+
+  // Filtering logic to pass to searchbar — Case & accent insensitive match
+  const filterByName = (products: ProductType[], name: string): ProductType[] =>
+    products.filter(product => {
+      return normaliseString(product.name).includes(
+        normaliseString(name).trim()
+      )
+    })
+
+  return (
+    <>
+      <Searchbar
+        items={allProducts}
+        setItems={setFilteredProducts}
+        searchLogic={filterByName as () => ProductType[]}
+      />
+      <div className={styles.productList}>
+        {!filteredProducts.length ? (
+          <div className={styles.notFoundContainer}>
+            <h2 className={styles.notFound}>
+              Aucun produit ne correspond à votre recherche.
+            </h2>
+            <NotFoundImage />
+          </div>
+        ) : (
+          filteredProducts
+            .sort(sortByProperty("name"))
+            .map((product, id) => (
+              <Product
+                key={id}
+                name={product.name}
+                price={product.price}
+                quantity={product.quantity}
+                quantityUnit={product.quantity_unit}
+              />
+            ))
+        )}
+      </div>
+    </>
+  )
+}
+
+export default ProductList
+function useSelector(arg0: (state: any) => any): { cart: any } {
+  throw new Error("Function not implemented.")
+}

476c956e44e95493ec78548add84bfdb22a3c4c6
YHR <h_yacine@live.fr>
2022-07-03 19:45:48 +0200

    Add Sass config + append utilities.scss + allow image ressources from `www.omie.fr`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require("path")
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `@import "utilities.scss";`, // Scss code that'd be prepended to every single scss file.
+    images: {
+      domains: ["www.omie.fr"], // Enables NextJS to serve images from external domains.
+    },
+  },
 }
 
 module.exports = nextConfig
